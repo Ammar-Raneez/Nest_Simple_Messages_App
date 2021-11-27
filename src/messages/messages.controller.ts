667de2dfbb052b1c686cd05449fda21b82db5f5b
ammar <ammarraneez@gmail.com>
2021-11-27 10:14:21 +0530

    feat: handle not found message

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,22 +1,32 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, NotFoundException } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  constructor(
+    private readonly messageService: MessagesService
+  ) { }
+
   @Get()
   listMessages() {
-
+    return this.messageService.findAll();
   }
 
   // method decorator
   @Post()
   // argument decorator - extract body & place it in a body variable
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messageService.create(body.content);
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    console.log(id);
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messageService.findOne(id);
+    if (!message) {
+      throw new NotFoundException('Message not found');
+    }
+
+    return message;
   }
 }
